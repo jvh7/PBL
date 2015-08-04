d3e12f3c06ef47c7bf10a2a0abbd0705aa5ba7da
jvh7 <jvh7@users.noreply.github.com>
2015-08-04 10:28:38 +0100

    Create test_code.r

---
+++ test_code.r
@@ -0,0 +1,10 @@
+# extract required variables
+station <- metData[,1]
+year <- metData[,2]
+doy <- metData[,3]
+hour <- metData[,4]
+toc <- metData[,5]
+u <- metData[,6]
+phi <- metData[,7]
+cloud <- metData[,8]
+cloudqc <- metData[,9]
